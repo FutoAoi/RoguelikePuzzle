1de18ec00083e01b6e47155620991c38e12dae52
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-10-06 16:38:37 +0900

    [add]盤面(仮)

--- Assets/Kobayashi/Scripts/GameManager.cs
+++ Assets/Kobayashi/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     [SerializeField] private TileHand _tileHand;
+    
 
     private void Awake()
     {
