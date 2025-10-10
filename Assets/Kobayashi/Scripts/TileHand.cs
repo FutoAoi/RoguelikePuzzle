efb9ba30c86ca9442c85c663fb1c9ec26945c477
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-10-10 18:06:27 +0900

    [add]ドラックアンドドロップ機能実装

--- Assets/Kobayashi/Scripts/TileHand.cs
+++ Assets/Kobayashi/Scripts/TileHand.cs
@@ -1,8 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 /// <summary>
 /// ŽèŽD
 /// </summary>
