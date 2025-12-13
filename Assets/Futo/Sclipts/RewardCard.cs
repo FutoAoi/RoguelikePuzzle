d07630ef777cc775aed9c30f951048bf68cd1008
FutoAoi <Faoi0803@icloud.com>
2025-12-13 15:00:24 +0900

    [Way]報酬機能途中

--- Assets/Futo/Sclipts/RewardCard.cs
+++ Assets/Futo/Sclipts/RewardCard.cs
@@ -1,21 +1,40 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class RewardCard : MonoBehaviour
+public class RewardCard : MonoBehaviour, IPointerDownHandler
 {
     [Header("カード詳細")]
     [SerializeField, Tooltip("カードのレアリティ")] private CardRarity _rarity;
-    [SerializeField, Tooltip("カードの見た目")] private Sprite _sprite;
-    [SerializeField, Tooltip("カードの名前")] private string _name;
-    [SerializeField, Tooltip("カードの説明")] private string _description;
-    [SerializeField, Tooltip("カードのコスト")] private int _cost;
-    [SerializeField, Tooltip("最大回数")] private int _maxTimes;
+    [SerializeField, Tooltip("カードID")] private int _cardID;
+    [SerializeField, Tooltip("カードの見た目")] private Image _image;
+    [SerializeField, Tooltip("カードの名前")] private TMP_Text _name;
+    [SerializeField, Tooltip("カードの説明")] private TMP_Text _description;
+    [SerializeField, Tooltip("カードのコスト")] private TMP_Text _cost;
+    [SerializeField, Tooltip("最大回数")] private TMP_Text _maxTimes;
+    [SerializeField, Tooltip("報酬番号")] private int _rewardNumber;
+    [SerializeField, Tooltip("RewardManager")] private RewardManager _rewardManager;
+
+    public int CardID => _cardID;
 
     private CardData _data;
 
-    private void SetCard(int ID)
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _rewardManager.SetRewardNumber(_rewardNumber);
+    }
+
+    public void SetCard(int ID)
     {
         _data = GameManager.Instance.CardDataBase.GetCardData(ID);
+        _cardID = ID;
         _rarity = _data.Rarity;
-        _sprite = _data.Sprite;
+        _image.sprite = _data.Sprite;
+        _name.text = _data.Name;
+        _description.text = _data.Description;
+        _cost.text = $"{_data.Cost}";
+        _maxTimes.text = $"{_data.MaxTimes}";
     }
+
 }
