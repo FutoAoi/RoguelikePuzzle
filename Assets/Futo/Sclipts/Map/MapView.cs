a59bee8bbe9d7fbb7daf2dd139bdf87483a65ca9
FutoAoi <Faoi0803@icloud.com>
2025-12-17 19:20:04 +0900

    [Add]マップジェネレーターの改変

--- Assets/Futo/Sclipts/Map/MapView.cs
+++ Assets/Futo/Sclipts/Map/MapView.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class MapView : MonoBehaviour
 {
     [SerializeField] private Room _roomPrefab;
-    [SerializeField] private LineRenderer _linePrefab;
+    [SerializeField] private RectTransform _linePrefab;
     [SerializeField] private MapManager _mapManager;
 
     private Room[][] _roomViews;
@@ -24,7 +24,9 @@ public class MapView : MonoBehaviour
                 Room room = Instantiate(_roomPrefab,transform);
                 room.SetRoomData(floorData.Rooms[r],_mapManager);
 
-                room.transform.localPosition = GetRoomPosition(f, r, roomCount);
+                RectTransform rt = room.GetComponent<RectTransform>();
+
+                rt.anchoredPosition = GetRoomPosition(f, r, roomCount);
 
                 _roomViews[f][r] = room;
             }
@@ -45,32 +47,46 @@ public class MapView : MonoBehaviour
                 _roomViews[f][r].SetNextRoom(nextRooms.ToArray());
             }
         }
-
         for (int f = 0; f < floorCount - 1; f++)
         {
             for (int r = 0; r < _roomViews[f].Length; r++)
             {
-                GenerateRoomData data =
-                    mapData.Floors[f].Rooms[r];
+                GenerateRoomData data = mapData.Floors[f].Rooms[r];
 
                 foreach (int next in data.NextRoomIndices)
                 {
-                    DrawLine(
-                        _roomViews[f][r].transform,
-                        _roomViews[f + 1][next].transform
+                    DrawUILine(
+                        _roomViews[f][r].GetComponent<RectTransform>(),
+                        _roomViews[f + 1][next].GetComponent<RectTransform>()
                     );
                 }
             }
         }
     }
 
-    private void DrawLine(Transform from, Transform to)
+    // ---------- UI用ライン描画 ----------
+    private void DrawUILine(RectTransform from, RectTransform to)
     {
-        LineRenderer line = Instantiate(_linePrefab, transform);
+        RectTransform line = Instantiate(_linePrefab, transform);
 
-        line.positionCount = 2;
-        line.SetPosition(0, from.position);
-        line.SetPosition(1, to.position);
+        Vector2 start = from.anchoredPosition;
+        Vector2 end = to.anchoredPosition;
+
+        Vector2 dir = end - start;
+        float distance = dir.magnitude;
+
+        // 位置：中点
+        line.anchoredPosition = start + dir * 0.5f;
+
+        // 長さ
+        line.sizeDelta = new Vector2(
+            line.sizeDelta.x,
+            distance
+        );
+
+        // 回転
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        line.localRotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 
     private Vector3 GetRoomPosition(int floorIndex, int roomIndex, int roomCount)
@@ -78,10 +94,11 @@ public class MapView : MonoBehaviour
         float xSpacing = 200f;
         float ySpacing = 200f;
 
-        float offsetX = -(roomCount - 1) * xSpacing * 2f;
+        float offsetX = -(roomCount - 1) * xSpacing * 0.5f;
+        float offsetY = -700f;
 
         float x = offsetX + roomIndex * xSpacing;
-        float y = floorIndex * ySpacing;
+        float y = offsetY + floorIndex * ySpacing;
 
         return new Vector3(x, y, 0f);
     }
