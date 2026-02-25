839a563daff1042b6bfe0f0892e149093a98ac14
FutoAoi <Faoi0803@icloud.com>
2026-02-25 21:53:11 +0900

    [Update]バージョンを6000.3.8f1に変更、カードデータに霊陣・破棄のboolを追加、エネミーに各ステータスを追加

--- Assets/Futo/Sclipts/DataBase/CardData.cs
+++ Assets/Futo/Sclipts/DataBase/CardData.cs
@@ -7,12 +7,15 @@ public class CardData : ScriptableObject
     [Header("カード詳細")]
     [SerializeField, Tooltip("カードID")] private int _cardID;
     [SerializeField, Tooltip("カードのレアリティ")] private CardRarity _rarity;
+    [SerializeField, Tooltip("カードのタイプ")] private CardType _type;
     [SerializeField, Tooltip("カードの見た目")] private Sprite _sprite;
     [SerializeField, Tooltip("カードの名前")] private string _name;
     [TextArea(3, 10)]
     [SerializeField, Tooltip("カードの説明")] private string _description;
     [SerializeField, Tooltip("カードのコスト")] private int _cost;
     [SerializeField, Tooltip("最大回数")] private int _maxTimes;
+    [SerializeField, Tooltip("霊陣")] private bool _isGhost = false;
+    [SerializeField, Tooltip("破棄")] private bool _isDestruction = false;
 
     [SerializeReference, SubclassSelector] private IEffect[] _effect;
 
@@ -24,4 +27,7 @@ public class CardData : ScriptableObject
     public IEffect[] Effect => _effect;
     public int MaxTimes => _maxTimes;
     public CardRarity Rarity => _rarity;
+    public CardType Type => _type;
+    public bool IsGhost => _isGhost;
+    public bool IsDestruction => _isDestruction;
 }
